003c60a9ebb93d96062b57dae86ceb1164368807
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-11-07 16:30:25 +0000

    Update server by adding access-control-origin headers to allow work with react client in local environment

--- src/server.js
+++ src/server.js
@@ -10,6 +10,16 @@ import schema from './graphql/schema';
 
 const app = express();
 
+app.use('/graphql', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(
     '/graphql', 
     graphqlHTTP((request, response, graphQLParams) => ({
