bce92a262efef26cfd17a7ec565dc18fe4b90d9b
ddialar <ddialar@gmail.com>
2017-11-07 11:29:26 +0000

    feat(graphql.queries.root.query): Implement the basic template for the file.

---
+++ src/graphql/queries/root.query.js
@@ -0,0 +1,25 @@
+import { GraphQLObjectType } from 'graphql';
+
+import * as DirectorsQuery from './directors.query';
+
+var RootQueryType = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: () => ({
+        allDirectors: DirectorsQuery.allQuery,
+        // directorsById: DirectorsQuery.byIdQuery,
+
+        // allWriters: WritersQuery.allQuery,
+        // writersById: WritersQuery.byIdQuery,
+
+        // allActors: ActorsQuery.allQuery,
+        // actorsById: ActorsQuery.byIdQuery,
+
+        // allGenres: GenresQuery.allQuery,
+        // genresById: GenresQuery.byIdQuery,
+
+        // allMovies: MoviesQuery.allQuery,
+        // moviesById: MoviesQuery.byIdQuery,
+    })
+});
+
+export default RootQueryType;
