568304a73df3e64990c1ebfe01333841bb93c3c5
ddialar <ddialar@gmail.com>
2017-11-07 11:34:59 +0000

    chore(graphql.queries.directors.query): Prepare the code in order to implement the 'byIdQuery' query.

--- src/graphql/queries/directors.query.js
+++ src/graphql/queries/directors.query.js
@@ -16,6 +16,33 @@ var allQuery = {
     }
 };
 
-export {
-    allQuery
+// En esta fase del proyecto, debes definir qué objeto tipo debe devolver
+// la consulta, así como la operación necesaria para obtener dicho objetivo.
+
+var byIdQuery = {
+    type: new GraphQLList(DirectorType),
+    description: 'List of all stored directors, filtered by their IDs.',
+    args: {
+        tarea: 'Definir los argumentos que va a recibir la consulta'
+    },
+    resolve: async (parentValues, args) => {
+
+        // Ahora vamos a ver qué recibimos como 'args' cuando hacemos una petición.
+
+        console.log(`Received args: ${JSON.stringify(args)}`);
+        return [];
+        
+        // let queryParams = utils.createQueryParamsString(args.id, 'id');
+        // return await DirectorsService.getDirectorsData(queryParams);
+    }
 };
+
+export {
+    allQuery,
+    byIdQuery
+};
+
+/*
+Solución
+id: { type: new GraphQLList(GraphQLInt) }
+*/
