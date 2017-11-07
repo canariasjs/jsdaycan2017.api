65576efe3559f18947706f42cf604513c70cc0a4
ddialar <ddialar@gmail.com>
2017-11-07 11:31:10 +0000

    refactor(server): Edit the server code in order to work with the GraphQL schema.

--- src/server.js
+++ src/server.js
@@ -10,12 +10,18 @@ var logger = log4js.getLogger('server');
 
 import { environment, serverConf }  from './config';
 
+import schema from './graphql/schema';
+
 const app = express();
+
+// Como paso final antes de poder realizar nuestra primera consulta,
+// debemos decirle al middleware de GrpahQL qué shcema vamos a usar.
 
 app.use(
     '/graphql', 
     graphqlHTTP((request, response, graphQLParams) => ({
-        graphiql: (environment.match('development')) ? true : false
+        graphiql: (environment.match('development')) ? true : false,
+        schema: 'Añadir el shcema del proyecto aquí'
     }))
 );
 
@@ -28,3 +34,8 @@ app.listen(
         logger.info(`App running on ${environment.toUpperCase()} mode and listening on port ${serverConf.api_gql.port} ...`);
     }
 );
+
+/*
+Solución:
+schema: schema
+*/
